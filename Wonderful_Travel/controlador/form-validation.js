792e93e468791d703ff05ee48154adf1da6aaf83
EliasEJ <e.jerari@sapalomera.cat>
2023-11-13 15:44:51 +0100

    Implementació rellotge analogic

--- Wonderful_Travel/controlador/form-validation.js
+++ Wonderful_Travel/controlador/form-validation.js
@@ -89,82 +89,50 @@ function validarData() {
   }
 }
 
-var canvas = document.getElementById("canvas_reloj");
-var objdibujo = canvas.getContext("2d");
-var radio = (canvas.height / 2);
-objdibujo.translate(radio, radio);
-radio = radio * 0.8;
-setInterval(reloj_run, 1000); 
 
-function reloj_run()
-{
-    frente_reloj	(objdibujo, radio);
-    numeros_reloj	(objdibujo, radio);
-    tiempo_reloj	(objdibujo, radio);
-}
+const deg = 6;
+const hour = document.querySelector(".hour");
+const min = document.querySelector(".min");
+const sec = document.querySelector(".sec");
 
-function frente_reloj(objdibujo, radio)
-{
-    var editarcolor;
-    objdibujo.beginPath();
-    objdibujo.arc(0,0,radio,0,2*Math.PI);
-    objdibujo.fillStyle = "white";
-    objdibujo.fill();
-    editarcolor = objdibujo.createRadialGradient(0,0,radio*0.95, 0,0,radio*1.05);
-    editarcolor.addColorStop(0.01, 'black');
-    editarcolor.addColorStop(0.1, 'white');
-	editarcolor.addColorStop(0.9, 'grey');
-	editarcolor.addColorStop(0.3, 'black');
-    objdibujo.strokeStyle = editarcolor;
-    // objdibujo.lineWidth = radio*0.01;
-	objdibujo.lineWidth = radio*0.1
-    objdibujo.stroke(); 
-    objdibujo.beginPath();
-    objdibujo.arc(0,0, radio*0.1,0,2*Math.PI);
-    objdibujo.fillStyle = 'black';
-    objdibujo.fill();
-}
+const setClock = () => {
+	let day = new Date();
+	let hh = day.getHours() * 30;
+	let mm = day.getMinutes() * deg;
+	let ss = day.getSeconds() * deg;
 
-function numeros_reloj(objdibujo, radio) {
-    var angulox;
-    var numerox;
-    objdibujo.font = radio + "% arial";
-    objdibujo.textBaseline = "middle";
-    objdibujo.textAlign = "center";
-    for(numerox=1; numerox < 13; numerox++)
-	{ 
-        angulox = numerox * Math.PI /6;
-        objdibujo.rotate	(angulox);
-        objdibujo.translate	(0, -radio*0.85);
-        objdibujo.rotate	(-angulox);
-        objdibujo.fillText	(numerox.toString(), 0, 0);
-        objdibujo.rotate(angulox);
-        objdibujo.translate(0, radio*0.85);
-        objdibujo.rotate(-angulox);
-    }
-}
+	hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+	min.style.transform = `rotateZ(${mm}deg)`;
+	sec.style.transform = `rotateZ(${ss}deg)`;
+};
 
-function tiempo_reloj(objdibujo, radio){
-    var tiempo_actual = new Date();
-    var hora = tiempo_actual.getHours();
-    var minutos = tiempo_actual.getMinutes();
-    var segundos = tiempo_actual.getSeconds();
-    hora = hora%12;
-    hora = (hora*Math.PI/6)+(minutos*Math.PI/(6*60))+(segundos*Math.PI/(360*60));
-    manecillas_reloj(objdibujo, hora, radio*0.5, radio*0.05);
-    minutos=(minutos*Math.PI/30)+(segundos*Math.PI/(30*60));
-    manecillas_reloj(objdibujo, minutos, radio*0.7, radio*0.05);
-    segundos=(segundos*Math.PI/30);
-    manecillas_reloj(objdibujo, segundos, radio*0.9, radio*0.02);
-}
+// first time
+setClock();
+// Update every 1000 ms
+setInterval(setClock, 1000);
 
-function manecillas_reloj(objdibujo, a, b, ancho){
-    objdibujo.beginPath();
-    objdibujo.lineWidth = ancho;
-    objdibujo.lineCap = "round";
-    objdibujo.moveTo(0,0);
-    objdibujo.rotate(a);
-    objdibujo.lineTo(0, -b);
-    objdibujo.stroke();
-    objdibujo.rotate(-a);
+const switchTheme = (evt) => {
+	const switchBtn = evt.target;
+	if (switchBtn.textContent.toLowerCase() === "light") {
+		switchBtn.textContent = "dark";
+		// localStorage.setItem("theme", "dark");
+		document.documentElement.setAttribute("data-theme", "dark");
+	} else {
+		switchBtn.textContent = "light";
+		// localStorage.setItem("theme", "light"); //add this
+		document.documentElement.setAttribute("data-theme", "light");
+	}
+};
+
+const switchModeBtn = document.querySelector(".switch-btn");
+switchModeBtn.addEventListener("click", switchTheme, false);
+
+let currentTheme = "dark";
+// currentTheme = localStorage.getItem("theme")
+// 	? localStorage.getItem("theme")
+// 	: null;
+
+if (currentTheme) {
+	document.documentElement.setAttribute("data-theme", currentTheme);
+	switchModeBtn.textContent = currentTheme;
 }
--- Wonderful_Travel/index.php
+++ Wonderful_Travel/index.php
@@ -1,122 +1,129 @@
-<?php require_once 'model/consultes_sql.php'?>
+<?php require_once 'model/consultes_sql.php' ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.84.0">
-    <title>Wonderful Travel</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.84.0">
+  <title>Wonderful Travel</title>
 
-    
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
 
-    <!-- Bootstrap core CSS -->
-<link href="estils/bootstrap.min.css" rel="stylesheet">
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
+
+  <!-- Bootstrap core CSS -->
+  <link href="estils/bootstrap.min.css" rel="stylesheet">
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
       }
+    }
+  </style>
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
+  <!-- Custom styles for this template -->
+  <link href="estils/form-validation.css" rel="stylesheet">
+</head>
 
-    
-    <!-- Custom styles for this template -->
-    <link href="estils/form-validation.css" rel="stylesheet">
-  </head>
+<body class="bg-light">
 
-  <body class="bg-light">
-    
-<div class="container">
-  <main>
-    <div class="py-5 text-center">
-      <h2>Wonderful Travel</h2>
-    </div>
-    <canvas id="canvas_reloj" class="reloj"></canvas>
-
-    <div class="row g-5">
-      <div class="col-md-5 col-lg-4 order-md-last">
-        <h4 class="d-flex justify-content-between align-items-center mb-3">
-          <span class="text-primary">Your cart</span>
-          <span class="badge bg-primary rounded-pill">3</span>
-        </h4>
-        <ul class="list-group mb-3">
-          <li class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-              <h6 class="my-0">Product name</h6>
-              <small class="text-muted">Brief description</small>
-            </div>
-            <span class="text-muted">$12</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-              <h6 class="my-0">Second product</h6>
-              <small class="text-muted">Brief description</small>
-            </div>
-            <span class="text-muted">$8</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-              <h6 class="my-0">Third item</h6>
-              <small class="text-muted">Brief description</small>
-            </div>
-            <span class="text-muted">$5</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between bg-light">
-            <div class="text-success">
-              <h6 class="my-0">Promo code</h6>
-              <small>EXAMPLECODE</small>
-            </div>
-            <span class="text-success">−$5</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between">
-            <span>Total (USD)</span>
-            <strong>$20</strong>
-          </li>
-        </ul>
-
-        <form class="card p-2">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Promo code">
-            <button type="submit" class="btn btn-secondary">Redeem</button>
-          </div>
-        </form>
+  <div class="container">
+    <main>
+      <div class="py-5 text-center">
+        <h2>Wonderful Travel</h2>
       </div>
-      <div class="col-md-7 col-lg-8">
-        <h4 class="mb-3">Dades del viatge</h4>
+        <div class="clock">
+          <div class="hour"></div>
+          <div class="min"></div>
+          <div class="sec"></div>
+        </div>
+        <div class="switch-cont">
+          <button class="switch-btn"> Light </button>
+      </div>
 
-        <!-- Formulari principal dades viatge. -->
-        <form class="needs-validation" novalidate>
+      <div class="row g-5">
+        <div class="col-md-5 col-lg-4 order-md-last">
+          <h4 class="d-flex justify-content-between align-items-center mb-3">
+            <span class="text-primary">Your cart</span>
+            <span class="badge bg-primary rounded-pill">3</span>
+          </h4>
+          <ul class="list-group mb-3">
+            <li class="list-group-item d-flex justify-content-between lh-sm">
+              <div>
+                <h6 class="my-0">Product name</h6>
+                <small class="text-muted">Brief description</small>
+              </div>
+              <span class="text-muted">$12</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between lh-sm">
+              <div>
+                <h6 class="my-0">Second product</h6>
+                <small class="text-muted">Brief description</small>
+              </div>
+              <span class="text-muted">$8</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between lh-sm">
+              <div>
+                <h6 class="my-0">Third item</h6>
+                <small class="text-muted">Brief description</small>
+              </div>
+              <span class="text-muted">$5</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between bg-light">
+              <div class="text-success">
+                <h6 class="my-0">Promo code</h6>
+                <small>EXAMPLECODE</small>
+              </div>
+              <span class="text-success">−$5</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between">
+              <span>Total (USD)</span>
+              <strong>$20</strong>
+            </li>
+          </ul>
 
-          <div class="row g-3">
-            <div class="col-sm-6">
-              <label for="data" class="form-label">Data</label>
-              <input type="date" class="form-control" id="data" value="" required oninput='validarData()'>
+          <form class="card p-2">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Promo code">
+              <button type="submit" class="btn btn-secondary">Redeem</button>
             </div>
-            
-            <div class="col-md-5">
-              <label for="destiLabel" class="form-label">Desti</label>
-              <select class="form-select" name="desti" id="desti" required onchange="canviDesti(), this.form.submit()">
-                <option value="">Selecciona desti</option>
-                <option value="europa" <?php if(isset($_GET["desti"]) && $_GET["desti"] == "europa") echo 'selected'; ?>>Europa</option>
-                <option value="asia" <?php if(isset($_GET["desti"]) && $_GET["desti"] == "asia") echo 'selected'; ?>>Asia</option>
-                <option value="america" <?php if(isset($_GET["desti"]) && $_GET["desti"] == "america") echo 'selected'; ?>>Amèrica</option>
-                <option value="africa" <?php if(isset($_GET["desti"]) && $_GET["desti"] == "africa") echo 'selected'; ?>>Àfrica</option>
-                <option value="oceania" <?php if(isset($_GET["desti"]) && $_GET["desti"] == "oceania") echo 'selected'; ?>>Oceania</option>
-              </select>
-            </div>
+          </form>
+        </div>
+        <div class="col-md-7 col-lg-8">
+          <h4 class="mb-3">Dades del viatge</h4>
+
+          <!-- Formulari principal dades viatge. -->
+          <form class="needs-validation" novalidate>
+
+            <div class="row g-3">
+              <div class="col-sm-6">
+                <label for="data" class="form-label">Data</label>
+                <input type="date" class="form-control" id="data" value="" required oninput='validarData()'>
+              </div>
+
+              <div class="col-md-5">
+                <label for="destiLabel" class="form-label">Desti</label>
+                <select class="form-select" name="desti" id="desti" required onchange="canviDesti()">
+                  <option value="">Selecciona desti</option>
+                  <option value="europa" <?php if (isset($_GET["desti"]) && $_GET["desti"] == "europa") echo 'selected'; ?>>Europa</option>
+                  <option value="asia" <?php if (isset($_GET["desti"]) && $_GET["desti"] == "asia") echo 'selected'; ?>>Asia</option>
+                  <option value="america" <?php if (isset($_GET["desti"]) && $_GET["desti"] == "america") echo 'selected'; ?>>Amèrica</option>
+                  <option value="africa" <?php if (isset($_GET["desti"]) && $_GET["desti"] == "africa") echo 'selected'; ?>>Àfrica</option>
+                  <option value="oceania" <?php if (isset($_GET["desti"]) && $_GET["desti"] == "oceania") echo 'selected'; ?>>Oceania</option>
+                </select>
+              </div>
 
               <div class="col-md-5">
                 <label for="destiPaisLabel" class="form-label"><br></label>
@@ -126,8 +133,10 @@
 
               <div class="col-md-5">
                 <label for="imgDesti" class="form-label"><br></label>
-                <?php //imatgePais();?>
-                <img src="<?php //imatgePais();?>" id="imgDesti" class="img-fluid" alt="Imatge destí">
+                <?php //imatgePais();
+                ?>
+                <img src="<?php //imatgePais();
+                          ?>" id="imgDesti" class="img-fluid" alt="Imatge destí">
               </div>
             </div>
 
@@ -157,25 +166,26 @@
               <input type="number" class="form-control" id="numPersones" placeholder="" required>
             </div>
             <br>
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="descompte">
-            <label class="form-check-label" for="descompte">Descompte 20%</label>
-          </div>
-          <br>
-          <button class="w-100 btn btn-primary btn-lg" type="submit">Afegir</button>
-          <hr class="my-4">
-        </form>
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" id="descompte">
+              <label class="form-check-label" for="descompte">Descompte 20%</label>
+            </div>
+            <br>
+            <button class="w-100 btn btn-primary btn-lg" type="submit">Afegir</button>
+            <hr class="my-4">
+          </form>
 
 
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
 
-  <footer class="my-5 pt-5 text-muted text-center text-small">
+    <footer class="my-5 pt-5 text-muted text-center text-small">
 
-  </footer>
-</div>
+    </footer>
+  </div>
 
-      <script src="controlador/form-validation.js"></script>
-  </body>
+  <script src="controlador/form-validation.js"></script>
+</body>
+
 </html>
